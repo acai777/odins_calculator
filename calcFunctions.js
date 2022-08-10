3decdee61ed63f75f8d15a15e13e5b393596700e
Arno Cai <acai367@gmail.com>
2022-08-10 18:35:14 -0400

    Create operate method

--- calcFunctions.js
+++ calcFunctions.js
@@ -19,3 +19,12 @@ function divide(a, b) {
     return a/b; 
 };
 
+// operate function. takes an operator and 2 numbers and then executes the functions on the two numbers
+function operate(operator, a, b) {
+    let methods = [add, subtract, multiply, divide]; // each of these are recognized as FUNCTIONS within JS. 
+    if (methods.includes(operator)) {
+        return operator(a, b);
+    }
+
+    // E.g.: with operate(add, 3, 4), the "add" is not recognized as a string, but as a function. 
+};

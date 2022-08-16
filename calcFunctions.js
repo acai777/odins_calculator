36f39fbcb7e097f059179043118029bf39f3d3c6
Arno Cai <acai367@gmail.com>
2022-08-16 11:57:26 -0400

    Create functions to populate display when click on number buttons

--- calcFunctions.js
+++ calcFunctions.js
@@ -28,3 +28,18 @@ function operate(operator, a, b) {
 
     // E.g.: with operate(add, 3, 4), the "add" is not recognized as a string, but as a function. 
 };
+
+// Create the functions that populate the display when you click the number buttons. Store the ‘display value’ in a variable somewhere.
+
+let displayDiv = document.getElementById('resultSection');
+let buttonClick = document.querySelectorAll('.number');
+buttonClick.forEach(button => {
+    button.addEventListener("mousedown", () => {
+        button.style.backgroundColor = "rgb(200,200,200)";
+        displayDiv.innerText = displayDiv.innerText+button.innerText;
+    });
+
+    button.addEventListener("mouseup", () => {
+        button.style.backgroundColor = "white";
+    });
+});
